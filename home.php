3469b5b323ee81c12de663df71d7c0c4d9d333fa
Elodie <elodie.gonay@gmail.com>
2015-01-07 15:43:38 +0100

    version en ligne

--- home.php
+++ home.php
@@ -8,24 +8,11 @@
 </head>
 <body>
 
+<?php include('header.php'); ?>
+		
 	<section id="slider">
 		
-		<header>
-			
-			<img id="logo" src="img/logomagic.png" alt="logo">
 
-				<nav>
-					<ul>
-						<li><h1>CATEGORIES</h1></li>
-						<li><h1>ABOUT</h1></li>
-						<li><h1>CART</h1></li>
-						<li>
-							<div  id="login"><h1>ACCOUNT / LOG IN</h1></div>
-						</li>
-					</ul>
-				</nav>
-						
-		</header>
 				<div id="text">
 					<h1>YOUR N°1 SOURCE</br> FOR MAGIC </br> TUTORIALS</h1>
 				</div>
@@ -66,35 +53,34 @@
 					<div class="containsearch">
 						<h1>Performance Style</h1>
 						<h2>Card Magic</h2>
-						<h2>Card Magic</h2>
-						<h2>Card Magic</h2>
-
+						<h2>Close-up magic</h2>
+						<h2>Mentalism</h2>
+						<h2>Parlor magic</h2>
+						<h2>Money magic</h2>
+						<h2>Magic Theory</h2>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="containsearch">
-						<h1>Props & Accesories</h1>
-						<h2>Card Magic</h2>
-						<h2>Card Magic</h2>
-						<h2>Card Magic</h2>
-						
+						<h1>Props & Accessories</h1>
+						<h2>Playing cards</h2>
+						<h2>Phoenix decks</h2>
+						<h2>Coin gimmicks</h2>
+						<h2>Prop Store</h2>
+						<h2>Custom close-up pads</h2>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="containsearch">
-						<h1>Browse by Product Style</h1>
-						<h2>Card Magic</h2>
-						<h2>Card Magic</h2>
+						<h1>Browse by product type</h1>
+						<h2>All magic books</h2>
+						<h2>All magic DVDs</h2>
+						<h2>Magic magazine</h2>
+						<h2>All products</h2>
 					</div>
 				</div>
 			</div>
 		</div>
-
-		
-		<div id="browsecat">
-			<h1>Browse all categories</h1>
-		</div>
-
 	</section>
 
 	<section id="videos">
@@ -123,17 +109,18 @@
 		<div class="container containvid">
 			<div class="row">
 				<div class="col-md-4">
+					<a href="video.php">
 					<div class="jm-item second"> <!-- HOVER VIDEO COMME SUR LES - AUTRES DESSOUS -->
 						<div class="jm-item-wrapper"> <!-- HOVER VIDEO -->
 							<div class="jm-item-image"> <!-- HOVER VIDEO -->
-								<img src="img/image1.jpg" alt="videocartes">
-								<div class="jm-item-description"> <!-- HOVER VIDEO -->
-								    <div class="jm-item-button"><a href="#">View</a></div> <!-- HOVER VIDEO -->
+								<a href="video.php"><img src="img/image1.jpg" alt="videocartes"></a>								<div class="jm-item-description"> <!-- HOVER VIDEO -->
+								    <div class="jm-item-button"><a href="video.php">View</a></div> <!-- HOVER VIDEO -->
 								</div> <!-- HOVER VIDEO -->
 							</div> <!-- HOVER VIDEO -->
 						</div> <!-- HOVER VIDEO -->
 					</div> <!-- HOVER VIDEO -->
-					<h1>Learn the best four ace trick</h1>
+					</a>
+					<h1><a href="video.php">Learn the best four ace trick</a></h1>
 					<h2>Length: 3:57</h2>
 				</div>
 				<div class="col-md-4">
@@ -212,11 +199,6 @@
 				</div>
 			</div>
 		</div>
-
-		<div class="allvideos">
-			<h1>WATCH ALL VIDEO</h1>
-		</div>
-
 	</section>
 
 	<section id="popularvid">
@@ -334,11 +316,6 @@
 				</div>
 			</div>
 		</div>
-		
-		<div class="allvideos">
-			<h1>WATCH ALL VIDEO</h1>
-		</div>
-
 	</section>
 
 	<section id="othercategories">
@@ -449,26 +426,8 @@
 		</div>
 
 	</section>
-
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<h1>Conditions légales</h1>
-
-				</div>
-				<div class="col-md-4">
-					<h1>Contact</h1>
-				</div>
-				<div class="col-md-4">
-					<h1>Help</h1>
-					<h2>FAQ</h2>
-				</div>
-			</div>
-		</div>
-
-	</footer>
 	
+	<?php include('footer.php'); ?>
 
 	<script src="js/bootstrap.min.js"></script>
 </body>
---
+++ video.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Magic University</title>
+	<link rel="stylesheet" href="style/bootstrap.css">
+	<link rel="stylesheet" href="style/stylevideo.css">
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+	<section id="video">
+	
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 ">
+						<div id="msnippet">
+							<img id="snippet" src="img/snippet.png" alt="snippet">
+						</div>
+
+					</div>
+					<div class="col-md-5 col-md-offset-1">
+						<div class="containsnippet">
+							<h1>New Tutorial : Easy amazing beginner magic card trick revealed in this tutorial</h1>
+							<div id="btnbot">
+								<h1>WATCH THE VIDEO</h1>
+							</div>
+							<h2>Download and get unlimited viewing</h2>
+							<h2>Money back guarantee</h2>
+							<h2>Source files included</h2>
+
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+			<section id="play">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="informations">
+								<h1>Lenght   Ratings</h1>								
+								<h2>38min23s</h2>
+								<br>								
+								<h1>Level of this tutorial</h1>								
+								<h2>Beginners</h2>
+								<br>								
+								<h1>Share this tutorial</h1>
+								<div id="twitterfb">
+									<img id="tuto" src="img/twitterfb.png" alt="tuto">
+								</div>								
+								<h1>Others Informations</h1>								
+								<h2>Money back guarantee</h2>								
+								<h2>Unlimited download</h2>								
+								<h2>Published on 30/11/14</h2>								
+								<h2>Download 324x</h2>								
+								<h2>Category: Card Tricks</h2>								
+								<h2>Language: French</h2>
+							</div>					
+						</div>
+
+						<div class="col-md-offset-1 col-md-8">
+							<div class="player">
+								<h1>In this tutorial you will learn how to easily do the Princess Card Trick</h1>
+								<div id="videocarte">
+									<img id="videocarte" src="img/videocarte.jpg" alt="videocarte">
+								</div>	
+								<p>The Princess Card Trick is first created on 1905 by American Magician Henry Hardin. In the years, this effect has been reinvented, renewed, rehased, and rewriten by more than a few. A great magiciane like Anneman, Dai Vernon, Ganson, Hillard, and Lance Burton once performed this trick in the past. So, you may wonder what is the Princess Card Trick is? It's quite simple trick, easy to perform, yet it's  very impressive.
+									<br/><br />
+									The Effect: The magician show packet of cards, normally four or five cards.
+									The spectator is requested to mentally remember one of the card. The cards are then turned back towards viewers and the magician pockets a single card. The faces of the remaining cards are shown and magically the spectator chosen card is gone!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<section id="trainer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-offset-4 col-md-8">
+							<div class="train">
+								<div class="trainer-presentation">
+									<img class="randomguy" src="img/randomguy.jpg" class="img-circle" alt="randomguy">
+								</div>			
+								<div class="extrain">
+									<h1>Trainer : John Doe</h1>				
+									<p>John Doe has published 9 tutorials already that you<br>can access to. He's got a rate of 4,5/5 on average.</p>
+									<h2>See John's other tutorials</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+
+			<section id="questions">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2 col-md-offset-6">
+							<div id="btnq">
+								<h1>QUESTIONS ABOUT THE TUTORIAL</h1>
+							</div>
+						</div>
+						
+						<div class="col-md-offset-4 col-md-8">
+							<div class="btnquest">
+								<img src="img/btnplus.png" alt="btnplus"> 
+								<h2> What is the required level for this tutorial?</h2>	
+							</div>
+							<div class="btnquest">
+								<img src="img/btnplus.png" alt="btnplus"> 
+								<h2> Do we get to have the source file included?</h2>	
+							</div>
+						</div>
+						
+						<div class="col-md-offset-4 col-md-8">
+							<div id="btnq2">
+								<h1>Ask the trainer another question</h1>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</section>
+
+			<section id="others">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-6">
+							<div class="lapin">
+								<div id="lapin">
+									<img id="lapin" src="img/lapin.png" class="img-circle" alt="randomguy">
+								</div>								
+							</div>					
+						</div>
+						<div class="col-md-10 col-md-offset-2">
+							<div id="container">
+								<div class="othersvid">
+									<h1>OTHER TRAINING SESSIONS YOU MIGHT BE INTERESTED IN</h1>		
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="othersvideos">
+								<div class="jm-item second"> <!-- HOVER VIDEO COMME SUR LES - AUTRES DESSOUS -->
+									<div class="jm-item-wrapper"> <!-- HOVER VIDEO -->
+										<div class="jm-item-image"> <!-- HOVER VIDEO -->
+											<img src="img/image1.png" alt="videocartes">
+											<div class="jm-item-description"> <!-- HOVER VIDEO -->
+												<div class="jm-item-button"><a href="#">View</a></div> <!-- HOVER VIDEO -->
+											</div> <!-- HOVER VIDEO -->
+										</div> <!-- HOVER VIDEO -->
+									</div> <!-- HOVER VIDEO -->
+								</div> <!-- HOVER VIDEO -->
+							</div>
+							<h2>Learn the best card trick</h2>
+							<p>Length: 3:54min</p>
+						</div>
+						<div class="col-md-4">
+							<div class="jm-item second"> 
+								<div class="jm-item-wrapper"> 
+									<div class="jm-item-image">
+										<img src="img/image2.png" alt="videocartes">
+										<div class="jm-item-description">
+											<div class="jm-item-button"><a href="#">View</a></div>
+										</div> 
+									</div>
+								</div>
+							</div>
+							<h2>Meteor 2</h2>
+							<p>Length: 3:54min</p>
+						</div>
+						<div class="col-md-4">
+							<div class="jm-item second">
+								<div class="jm-item-wrapper">
+									<div class="jm-item-image">
+										<img src="img/image3.png" alt="videocartes">
+										<div class="jm-item-description">
+											<div class="jm-item-button"><a href="#">View</a></div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<h2>Card Tricks</h2>
+							<p>Length: 3:54</p>
+						</div>
+						<div class="col-md-offset-4 col-md-4 col-md-offset-4">
+							<div id="points">
+								<img id="points" src="img/points.png" alt="points">
+							</div>	
+						</div>
+					</div>
+				</div>					
+			</section>
+
+			<?php include('footer.php'); ?>
+
+			<script src="js/bootstrap.min.js"></script>
+		</body>
+		</html>
